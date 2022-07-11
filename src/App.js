3ce2b661c8936afa4d2967a63758fb6ac7e4aa5d
jenna-graham <jclayto2@gmail.com>
2022-07-11 16:20:42 -0700

    finished assignment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 
 import Player from './Player';
 import CardList from './CardList';
-import { useState } from 'react';
 import ExecutePassButton from './ExecutePassButton';
 import { useCardContext } from './ContextProvider';
 
 function App() {
-  const { selectedCard } = useCardContext();
+  const { selectedCard, playerOneHand, playerTwoHand, playerThreeHand, deck } = useCardContext();
 
   // arrays start at zero, but our players start at 1 :shrug:
 
@@ -15,46 +14,41 @@ function App() {
     <div className="App">
       <section>
         <Player
-        // to={to}
-        // player={1}
-        // hand={playerOneHand}
-        // setFrom={setFrom}
-        // selectedCard={selectedCard}
-        // setTo={setTo}
-        // setSelectedCard={setSelectedCard}
+          player={1}
+          hand={playerOneHand}
+          // to={to}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
-        // to={to}
-        // player={2}
-        // hand={playerTwoHand}
-        // setFrom={setFrom}
-        // selectedCard={selectedCard}
-        // setTo={setTo}
-        // setSelectedCard={setSelectedCard}
+          player={2}
+          hand={playerTwoHand}
+          // to={to}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
-        // to={to}
-        // player={3}
-        // hand={playerThreeHand}
-        // setFrom={setFrom}
-        // selectedCard={selectedCard}
-        // setTo={setTo}
-        // setSelectedCard={setSelectedCard}
+          player={3}
+          hand={playerThreeHand}
+          // to={to}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
         <CardList
-        // cards={deck}
-        // selectedCard={selectedCard}
-        // setSelectedCard={setSelectedCard}
-        // setFrom={setFrom}
-        // player={'deck'}
+          cards={deck}
+          player={'deck'}
+          // selectedCard={selectedCard}
+          // setSelectedCard={setSelectedCard}
+          // setFrom={setFrom}
         />
       </section>
-      <section>
-        {selectedCard && (
-          <ExecutePassButton />
-          // passCard={passCard} from={from} to={to} selectedCard={selectedCard}
-        )}
-      </section>
+      <section>{selectedCard && <ExecutePassButton />}</section>
     </div>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import CardList from './CardList';
+import { useCardContext } from './ContextProvider';
 
-export default function Player({ player, setTo, hand, setFrom, setSelectedCard, selectedCard, to }) {
+export default function Player({ player, hand }) {
+  const { setTo, setFrom, setSelectedCard, selectedCard, to } = useCardContext();
   return (
-    <div className={`player ${to === player ? 'selected-player' : ''}`} onClick={() => setTo(player)}>
+    <div
+      className={`player ${to === player ? 'selected-player' : ''}`}
+      onClick={() => setTo(player)}
+    >
       <p>Player {player}</p>
       <CardList
         player={player}
         cards={hand}
         setFrom={setFrom}
         selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard} />
-      
+        setSelectedCard={setSelectedCard}
+      />
     </div>
   );
 }
